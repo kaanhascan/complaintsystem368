dc4d0a74b8a6adac162211004bc1ad7b157886a3
kaanhascan <kaan.hascan@std.yeditepe.edu.tr>
2024-05-29 14:17:21 +0300

    Adjustments

--- send_complaint.php
+++ send_complaint.php
@@ -11,23 +11,25 @@ if ($conn->connect_error) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(isset($_POST['Name']) && isset($_POST['email']) && isset($_POST['Subject']) && isset($_POST['Text'])) {
-        $name = $conn->real_escape_string($_POST['Name']);
+    if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['text'])) {
+        $name = $conn->real_escape_string($_POST['name']);
         $email = $conn->real_escape_string($_POST['email']);
-        $subject = $conn->real_escape_string($_POST['Subject']);
-        $text = $conn->real_escape_string($_POST['Text']);
-
+        $subject = $conn->real_escape_string($_POST['subject']);
+        $text = $conn->real_escape_string($_POST['text']);
 
         $sql = "INSERT INTO complaints (name, email, subject, text) VALUES ('$name', '$email', '$subject', '$text')";
-
 
         if ($conn->query($sql) === TRUE) {
             echo "Your complaint has been successfully sent!";
         } else {
-            echo "Error:" . $sql . "<br>" . $conn->error;
+            echo "Error: " . $sql . "<br>" . $conn->error;
         }
 
-
         $conn->close();
-    }}
+    } else {
+        echo "All fields are required!";
+    }
+} else {
+    echo "Invalid request method!";
+}
 ?>
